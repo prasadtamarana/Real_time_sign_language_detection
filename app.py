0f4bbdef1a4f71202835a7a78f7f964432386869
Prasadtamarana <prasadtamarana11@gmail.com>
2026-05-12 15:57:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ cap = cv2.VideoCapture(0)
 
 with mp_hands.Hands(
     static_image_mode=False,
-    max_num_hands=1,
+    max_num_hands=2,
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 ) as hands:
